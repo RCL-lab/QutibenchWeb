39463cd8a65f2f12f46d10ed56a21ef324b71156
zacharyneveu <s191344@student.dtu.dk>
2020-04-23 22:03:34 -0400

    try adding notebooks and config

---
+++ _notebooks/scripts/utils.py
@@ -0,0 +1,15 @@
+#hide
+def get_pareto_df(df, groupcol, xcol, ycol):
+    pareto_line_df = df.groupby(groupcol)[xcol].max().to_frame("x")
+    pareto_line_df['y'] = df.groupby(groupcol)[ycol].agg(lambda x: x.value_counts().index[0])
+    pareto_line_df.sort_values('y', ascending=False, inplace=True)
+    pareto_line_df['x'] = pareto_line_df.x.cummax()
+    pareto_line_df.drop_duplicates('x', keep='first', inplace=True)
+    pareto_line_df['group'] = pareto_line_df.index
+    return pareto_line_df
+
+def label_point(x, y, val, ax, rot=0):
+    """ from https://stackoverflow.com/questions/46027653/adding-labels-in-x-y-scatter-plot-with-seaborn"""
+    a = pd.concat({'x': x, 'y': y, 'val': val}, axis=1)
+    for i, point in a.iterrows():
+        ax.text(point['x']+.02, point['y'], str(point['val']), rotation=rot)

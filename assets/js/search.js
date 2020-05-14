3473cf4547164c463492e0b37d659f9337d96a68
alinavalinav <alinavalinav@users.noreply.github.com>
2020-05-14 15:58:14 +0000

    deploy: 391cfc0b5365dd6a145470b00fe1dc0a9a587eb8

--- assets/js/search.js
+++ assets/js/search.js
@@ -47,7 +47,7 @@ jtd.onReady = function(ready) {
 
 function initSearch() {
     var request = new XMLHttpRequest();
-    request.open('GET', '/Qutibench_Web/assets/js/search-data.json', true);
+    request.open('GET', '/QutibenchWeb/assets/js/search-data.json', true);
   
     request.onload = function(){
       if (request.status >= 200 && request.status < 400) {

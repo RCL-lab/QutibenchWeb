0d5018d29ffe0ab24611064894ea9e6965397a64
Alina Vasilciuc <alinav@xlnx.xilinx.com>
2020-06-09 15:18:48 +0100

    Theoretical Pareto plots for all Classification

--- _notebooks/scripts/overlapped_pareto.py
+++ _notebooks/scripts/overlapped_pareto.py
@@ -7,7 +7,80 @@ import altair as alt
 W = 600
 H = 480
 
+
+#-------------------------------------------------
+def process_theo_top1(csv_theor_accuracies: str) -> pd.DataFrame():
+    """
+    Method that gets the CNNs and their accuracies table from Theoretical Analysis and melts it into 2 columns 
+   
+    Parameters
+    ----------
+    csv_theor_accuracies:str
+        Filepath to the CNNs and their accuracy table. 
+    
+    Returns
+    -------
+    df_top1_theo: pd.DataFrame()
+        Datraframe with 2 columns: |top1 | net_prun_datatype|
+        
+    """
+    # GET THEORETICAL values
+    #  get the table above
+    df_top1_theo = pd.read_csv(csv_theor_accuracies)
+    #  melt it into 2 columns: 
+    df_top1_theo = melt_df(df_in= df_top1_theo, cnn_names_col= ' ', new_column_names=['net_prun','datatype','top1'])
+    #fix small stuff like deleting rows, merging columns...
+    df_top1_theo = fix_small_stuff_df(df= df_top1_theo, col_to_drop=['index','datatype','net_prun'] )
+    #  now we have: top1 | net_prun_datatype 
+    return df_top1_theo
+
 #------------------------------------------------------
+
+def process_theo_fps(df_top1_theo:pd.DataFrame(),csv_files: list) -> pd.DataFrame():
+    """
+    Method that gets the data from the csv of the Heatmap tables.
+    Merges this theoretical df with the given theoretical df (fps+top1) on the 'net_prun_datatype' common column.
+    Removes nans from the 'values' column. Changes column order and columns names.
+    Replaces things to match.
+    
+    Notes: Values on the shared column need to be equal for them to be included on the merge. 
+            Eg.: 'MLP_100%_INT2' has to match with 'MLP_100%_INT2' otherwise what comes from the performance precitions will be ignored
+ 
+    Parameters
+    ----------
+    csv_theor_accuracies:str
+        Filepath to the CNNs and their accuracy table. 
+    
+    Returns
+    -------
+    df_top1_theo: pd.DataFrame()
+        Datraframe with 2 columns: |top1 | net_prun_datatype|
+        
+    """
+    df_fps_theo = pd.DataFrame()
+    for csv_file in csv_files:
+        df_tmp = pd.read_csv(csv_file)
+        df_fps_theo = pd.concat([df_fps_theo, df_tmp])
+    df_fps_theo['x']= df_fps_theo['x'].str.replace('-','_')
+    #    remove rows that have 'nan' in the 'values' column
+    df_fps_theo = df_fps_theo[df_fps_theo['values'].notna()]
+    #    rename columns
+    df_fps_theo.columns=['hardw','net_prun_datatype','fps']
+
+    #   Merge both Theoretical dataframes: fps + top1 
+    df_fps_top1_theo = pd.merge(df_top1_theo, df_fps_theo, on='net_prun_datatype', how='outer')
+    #  change column order
+    df_fps_top1_theo = df_fps_top1_theo[['net_prun_datatype', 'hardw', 'top1', 'fps']]
+    #  change column names
+    df_fps_top1_theo.columns = ['net_prun_datatype', 'hardw_datatype', 'top1', 'fps-comp']
+
+    #Notes: 1. make sure everything in 'net_prun_datatype' column has network + prunning + datatype. If not it will fail
+    df_fps_top1_theo = replace_data_df(df_=df_fps_top1_theo, column= 'net_prun_datatype', list_tuples_data_to_replace= [('GoogLeNetv1','GoogLeNetv1_100%'),('MobileNetv1','MobileNetv1_100%'),('GoogleNetv1','GoogleNetv1_100%'), ('EfficientNet_S','EfficientNet-S_100%'), ('EfficientNet_M','EfficientNet-M_100%'), ('EfficientNet_L','EfficientNet-L_100%'), ('%','')])
+    #  now that we have: net_prun_datatype | hardw_datatype | top1 | fps-comp
+    return df_fps_top1_theo
+
+#----------------------------------------------------------------------
+
 def melt_df(df_in: pd.DataFrame(), cnn_names_col: str, new_column_names: list)->pd.DataFrame():
     """Melts a dataframe into 2 columns, the 'cnn_names_col' and the 'value' column. Return the melted dataframe"""
     df_out = pd.DataFrame()
@@ -79,7 +152,7 @@ def get_point_chart_simple(df: pd.DataFrame,
 def get_line_chart(df: pd.DataFrame, groupcol: str, xcol: str, ycol:str, color:str) ->alt.vegalite.v4.api.Chart:
     """Creates simple line chart"""
     return alt.Chart(df).interactive().mark_line(point=True).encode(
-        x=xcol,
+        x=alt.X(xcol, scale=alt.Scale(type='log')),
         y=alt.Y(ycol + ":Q", scale=alt.Scale(zero=False)),
         color=alt.value(color),
         tooltip=[groupcol, xcol, ycol],
@@ -130,7 +203,20 @@ def process_measured_df(df_theoret: pd.DataFrame(), csv_measured:str ):
 #-------------------------------------------------
 
 def select_cnn_match_theo_for_measured(df_theo: pd.DataFrame(), net_prun_datatype: str):
-    """This method creates an overlepped pareto"""
+    """
+        This method creates an overlepped pareto
+   
+    Parameters
+    ----------
+    csv_theor_accuracies:str
+        Filepath to the CNNs and their accuracy table. 
+    
+    Returns
+    -------
+    df_top1_theo: pd.DataFrame()
+        Datraframe with 2 columns: |top1 | net_prun_datatype|
+        
+    """
     #   create a subset from the given dataframe
     # there is another way to do this # df_theo = df_superset[df_superset.apply(lambda row: row[net_prun_datatype].split('_')[0] == cnn_keyword, axis=1)]
     #the line below is not needed because there is only 1 classification
@@ -173,48 +259,6 @@ def fix_small_stuff_df(df: pd.DataFrame(), col_to_drop: list, ) -> pd.DataFrame(
     #    separate by underscore instead of space
     df = replace_data_df(df_=df, column='net_prun_datatype', list_tuples_data_to_replace=[(' ','_')])
     return df
-
-#-------------------------------------------------
-
-def process_theo_top1(csv_theor_accuracies: str) -> pd.DataFrame():
-    # GET THEORETICAL values
-    #  get the table above
-    df_top1_theo = pd.read_csv(csv_theor_accuracies)
-    #  melt it into 2 columns: 
-    df_top1_theo = melt_df(df_in= df_top1_theo, cnn_names_col= ' ', new_column_names=['net_prun','datatype','top1'])
-    #fix small stuff like deleting rows, merging columns...
-    df_top1_theo = fix_small_stuff_df(df= df_top1_theo, col_to_drop=['index','datatype','net_prun'] )
-    #  now we have: top1 | net_prun_datatype 
-    return df_top1_theo
-
-#----------------------------------------------
-
-def process_theo_fps(df_top1_theo:pd.DataFrame(),csv_files: list) -> pd.DataFrame():
-    #    GET Heatmaps/Theoretical TABLES
-    # get fps data from csv from heatmaps and mrge them on the 'net_prun_datatype' common column.
-    # Notes: 1. values need to be the sam. 
-    #        Eg.: 'MLP_100%_INT2' has to match with 'MLP_100%_INT2' otherwise what comes from the performance precitions will be ignored
-    df_fps_theo = pd.DataFrame()
-    for csv_file in csv_files:
-        df_tmp = pd.read_csv(csv_file)
-        df_fps_theo = pd.concat([df_fps_theo, df_tmp])
-    df_fps_theo['x']= df_fps_theo['x'].str.replace('-','_')
-    #    remove rows that have 'nan' in the 'values' column
-    df_fps_theo = df_fps_theo[df_fps_theo['values'].notna()]
-    #    rename columns
-    df_fps_theo.columns=['hardw','net_prun_datatype','fps']
-
-    #   Merge both Theoretical dataframes: fps + top1 
-    df_fps_top1_theo = pd.merge(df_top1_theo, df_fps_theo, on='net_prun_datatype', how='outer')
-    #  change column order
-    df_fps_top1_theo = df_fps_top1_theo[['net_prun_datatype', 'hardw', 'top1', 'fps']]
-    #  change column names
-    df_fps_top1_theo.columns = ['net_prun_datatype', 'hardw_datatype', 'top1', 'fps-comp']
-
-    #Notes: 1. make sure everything in 'net_prun_datatype' column has network + prunning + datatype. If not it will fail
-    df_fps_top1_theo = replace_data_df(df_=df_fps_top1_theo, column= 'net_prun_datatype', list_tuples_data_to_replace= [('GoogLeNetv1','GoogLeNetv1_100%'),('MobileNetv1','MobileNetv1_100%'),('GoogleNetv1','GoogleNetv1_100%'), ('EfficientNet_S','EfficientNet-S_100%'), ('EfficientNet_M','EfficientNet-M_100%'), ('EfficientNet_L','EfficientNet-L_100%'), ('%','')])
-    #  now that we have: net_prun_datatype | hardw_datatype | top1 | fps-comp
-    return df_fps_top1_theo
 
 #--------------------------------------------------------
 
@@ -269,11 +313,9 @@ def get_overlapped_pareto(net_keyword: str):
     Returns
     -------
     Heatmap Chart: altair.vegalite.v4.api.Chart
-        This is an Altair/Vega-Lite Heatmap chart.    
-        
+        This is an Altair/Vega-Lite Heatmap chart. 
+        It returns the overlapped pareto plot (theoretical + measured + 2 pareto lines(theoretical+measured)).       
     """
-
-
     # 1. Get the CNNs Accuracies table (Theoretical_Analysis/CNNs and their accuracy...) that only has the top1 accuracy and process it.
     #   theoretical top1
     df_top1_theo = process_theo_top1(csv_theor_accuracies ='data/cnn_topologies_accuracy.csv')
@@ -310,4 +352,50 @@ def get_overlapped_pareto(net_keyword: str):
 
 #----------------------------------------------------
 
+def get_point_chart(df: pd.DataFrame, groupcol: str, xcol: str, ycol:str, title: str) ->alt.vegalite.v4.api.Chart:
+    """Creates simple line chart"""
+    points = alt.Chart(df).mark_point(filled=True).properties(
+            width= W,
+            height= 1.3*H,
+            title=title
+        ).encode(
+        x= alt.X(xcol, scale=alt.Scale(type='log')),
+        y= alt.Y(ycol + ":Q", scale=alt.Scale(zero=False)),
+        color= alt.Color(groupcol),
+        tooltip= [groupcol, xcol, ycol],
+    )
+    text = points.mark_text(
+        angle=325,
+        align='left',
+        baseline='middle',
+        dx=7
+    ).encode(
+        text='hardw'
+    )
+    return (points+text).interactive()
+#----------------------------------------------------
 
+def theor_pareto(net_keyword, title: str) ->pd.DataFrame:
+    """Creates a Theoretical Pareto Plot."""
+    xcol='fps-comp'
+    ycol='top1'
+    groupcol='hardw_datatype_net_prun'
+    colors=['#FFA500']
+    # 1. Get the CNNs Accuracies table (Theoretical_Analysis/CNNs and their accuracy...) that only has the top1 accuracy and process it.
+    #   theoretical top1  
+    df_top1_theo = process_theo_top1(csv_theor_accuracies ='data/cnn_topologies_accuracy.csv')
+    
+    if re.search(net_keyword, 'imagenet', re.IGNORECASE):           
+        df_fps_top1_theo = process_theo_fps(df_top1_theo= df_top1_theo, csv_files=["data/cleaned_csv/performance_prediction_imagenet.csv"])
+    elif re.search(net_keyword, 'mnist', re.IGNORECASE):   
+        df_fps_top1_theo = process_theo_fps(df_top1_theo= df_top1_theo, csv_files=["data/cleaned_csv/performance_prediction_mnist.csv"])
+    elif re.search(net_keyword, 'cifar-10', re.IGNORECASE):   
+        df_fps_top1_theo = process_theo_fps(df_top1_theo= df_top1_theo, csv_files=["data/cleaned_csv/performance_prediction_cifar10.csv"])     
+    
+    df_fps_top1_theo = select_cnn_match_theo_for_measured(df_theo= df_fps_top1_theo, net_prun_datatype = 'net_prun_datatype')
+    # now we have: |hardw_datatype_net_prun | hardw | network | fps-comp | top1 | type|
+    
+    
+    df_charts = get_several_paretos_df(list_df = [df_fps_top1_theo], xcol= xcol, ycol= ycol, groupcol= groupcol, colors=colors)
+    points = get_point_chart(df= df_fps_top1_theo, groupcol= groupcol, xcol= xcol, ycol=ycol, title=title) 
+    return (points + df_charts.iloc[0,0])

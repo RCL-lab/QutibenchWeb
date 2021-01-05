262500f60581b3fbbf78d4e35a4b2862b4c2a208
alinavalinav <60705229+alinavalinav@users.noreply.github.com>
2021-01-05 16:33:17 +0000

    Added units for the Raw measurements

--- _notebooks/scripts/script_load_save_data.py
+++ _notebooks/scripts/script_load_save_data.py
@@ -6,6 +6,15 @@ import csv
 #This script loads unprocessed csv's and processes them
 #This is done for the Rooflines and for the Heatmaps
 
+def change_column_names(df):
+    df.columns = ['NN_Topology', 'HWType', 'Datatype', 'Op mode', 'batch/thread/stream',
+       'lat-sys [msec]', 'lat-comp [msec]', 'fps-system [fps]', 'fps-comp [fps]', 'tp-system [TOP/sec]', 'tp-comp [TOP/sec]',
+       'top1 [%]', 'top5 [%]', 'Base_Pwr [W]', 'Idle_Pwr [W]', 'Full_Pwr [W]', 'GOPS',
+       'PruningFactor', ' level', 'hw_peak_perf [TOP/sec]', 'hw_bandwidth [GBps]',
+       'nn_total_operations', 'hw_datatype_prun_net', 'norm-lat-comp',
+       'datatype_model', 'tag']
+    return df
+    
 def get_df_by_column(filename, column):
     """
     This function:
@@ -26,6 +35,7 @@ def get_df_by_column(filename, column):
         Subsets of the bigger dataframe, subsetted by the column's unique values
     """    
     df = pd.read_csv(filename)
+    df = change_column_names(df) #this will change the column names to include the 
     unique_column_values = df[column].unique()
     dataframes = []
     for value in unique_column_values:
